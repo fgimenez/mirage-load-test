bbb0831c6a40838d0b1804b24345ebf6e069f4be
Federico Gimenez <fgimenez@coit.es>
2017-01-05 17:28:29 +0100

    fix opam packages list

--- image/scripts/mirage.sh
+++ image/scripts/mirage.sh
@@ -4,19 +4,6 @@ su -l -c "opam upgrade -y" mlt
 su -l -c "opam switch ${OCAML_VERSION}" mlt
 su -l -c "eval `opam config env`" mlt
 PACKAGES="
-cmdliner
-depext
-io-page
-lwt
-mirage
-mirage-block-unix
-mirage-block-xen
-mirage-types
-vhd-format
-xen-evtchn
-xen-gnt
-xenstore
-xenstore_transport
 "
-su -l -c "opam install -y $PACKAGES" mlt
+su -l -c "opam install -y cmdliner depext io-page lwt mirage mirage-block-unix mirage-block-xen mirage-types vhd-format xen-evtchn xen-gnt xenstore xenstore_transport" mlt
 su -l -c "git clone https://github.com/mirage/xen-disk /home/mlt/xen-disk" mlt

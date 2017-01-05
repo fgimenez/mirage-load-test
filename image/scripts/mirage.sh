921c12d27d448ba19dc96bf7674e7357202dfeb6
Federico Gimenez <fgimenez@coit.es>
2017-01-05 10:19:54 +0100

    added missing packages; back to OCaml 4.01.0

--- image/scripts/mirage.sh
+++ image/scripts/mirage.sh
@@ -3,5 +3,20 @@ su -l -c "opam update" mlt
 su -l -c "opam upgrade -y" mlt
 su -l -c "opam switch ${OCAML_VERSION}" mlt
 su -l -c "eval `opam config env`" mlt
-su -l -c "opam install -y cmdliner io-page lwt mirage-block-unix mirage-block-xen mirage-types vhd-format xen-evtchn xen-gnt xenstore xenstore_transport" mlt
+PACKAGES="
+cmdliner
+depext
+io-page
+lwt
+mirage
+mirage-block-unix
+mirage-block-xen
+mirage-types
+vhd-format
+xen-evtchn
+xen-gnt
+xenstore
+xenstore_transport
+"
+su -l -c "opam install -y $PACKAGES" mlt
 su -l -c "git clone https://github.com/mirage/xen-disk /home/mlt/xen-disk" mlt
